b216134b4bb2b5577ef746698b151dec28b00065
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-07-22 13:20:15 -0700

    Add batch files for environment setup and installation; update .gitignore

---
+++ 01_install.bat
@@ -0,0 +1,4 @@
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
+pip install -r requirements.txt
+pip install deepspeed-0.17.2+a0bb346-py3-none-any.whl
+pip install torch_cluster-1.6.3+pt27cu128-cp311-cp311-win_amd64.whl
---
+++ 02_serve.bat
@@ -0,0 +1,2 @@
+python app.py
+# python inference_partcrafter.py --image_path assets/images/np3_2f6ab901c5a84ed6bbdf85a67b22a2ee.png --num_parts 3 --tag robot
